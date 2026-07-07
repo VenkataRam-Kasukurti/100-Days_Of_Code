f1a67e2ef20ca2febe6adb9c2fbf9e3449e36e3e
VenkataRam-Kasukurti <venkata.kasukurti@arcadis.com>
2026-07-07 11:47:26 +0530

    Day01 Started

--- Day01/Learning_Basics/Functions.py
+++
@@ -1,1 +0,0 @@
-print("elcome to the Git Hub for the first time ")
---
+++ Day01/String_Manipulation.py
@@ -0,0 +1,8 @@
+# Learn to use the string concatenation and the new line escape sequence to format string in python
+print("hello world!\nHello world!")
+
+# 1. use \n to add another line "Hello world"
+print("hello world!\nHello world!\nHello world!")
+
+# Add a space between 2 strings
+print("Hello"+" "+"Venkat")
